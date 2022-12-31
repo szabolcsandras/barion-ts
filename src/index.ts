a18169d69916123912d7252b07829f84df5d4589
Andras Szabolcs <szabolcs@Andrass-MacBook-Pro.local>
2022-12-31 16:30:43 +0100

    bckp

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,33 @@
 import Barion from "./Barion";
+import BankCard from "./BankCard";
+import BarionError from "./BarionError";
+import DetailedPaymentTransaction from "./DetailedPaymentTransaction";
+import FundingInformation from "./FundingInformation";
+import NameInformationStructure from "./NameInformationStructure";
+import UserInformation from "./UserInformation";
+import RecurrenceType from "./RecurrenceType";
+import ProcessedTransaction from "./ProcessedTransaction";
+import PaymentTransaction from "./PaymentTransaction";
+import PaymentState from "./PaymentState";
+import PaymentResult from "./PaymentResult";
+import Payment from "./Payment";
+import PayeeTransaction from "./PayeeTransaction";
 
-export = Barion;
+export {
+    BankCard,
+    Barion,
+    BarionError,
+    DetailedPaymentTransaction,
+    FundingInformation,
+    NameInformationStructure,
+    PayeeTransaction,
+    Payment,
+    PaymentResult,
+    PaymentState,
+    PaymentTransaction,
+    ProcessedTransaction,
+    RecurrenceType,
+    UserInformation,
+};
+
+export default Barion;
